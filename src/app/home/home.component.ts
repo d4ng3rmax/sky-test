af992d3fed93ecc7e531599ebfc9cbe3cb647e4c
G.Silveira <gabriel.ch23@gmail.com>
2020-01-14 00:13:25 -0300

    last changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,18 +12,22 @@ export class HomeComponent implements OnInit {
   movies: Array<any>;
 
 
-  // https://stackoverflow.com/questions/49915051/angular-ngfor-create-table-with-group-item
-  groupArr = this.movies.reduce((r, { group }) => {
-    if (!r.some(o => o.group == group)) {
-      r.push({ group, groupItem: this.movies.filter(v => v.group == group) });
-    }
-    return r;
-  }, []);
+
 
   constructor(private movieService: MoviesService) { }
 
   ngOnInit() {
     this.listMovies();
+
+    // categories
+    // https://stackoverflow.com/questions/49915051/angular-ngfor-create-table-with-group-item
+
+    var groupArr = this.movies.reduce((r, { categories }) => {
+      if (!r.some(o => o.group == categories)) {
+        r.push({ categories, categoriesItem: this.movies.filter(v => v.categories == categories) });
+      }
+      return r;
+    }, []);
   }
 
   listMovies() {
